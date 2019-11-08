60ef9261cafe49d76c520d03eab2635f65b10d46
Geraud <geraudster@gmail.com>
2019-11-08 23:56:10 +0100

    Fix run script...

--- tshark/run.sh
+++ tshark/run.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 INTERFACE=${INTERFACE:-$(tshark -D | head -1 | cut -f2 -d' ')}
+LINES_BY_FILE=${LINES_BY_FILE:-100}
 TIMESTAMP=$(date '+%s')
-exec tshark -l -n -T ek -i $INTERFACE | grep -v '^{"index":' | split -l 100 - /logs/packets-${TIMESTAMP}-
+exec tshark -l -n -T ek -i $INTERFACE | grep -v '^{"index":' | split -l ${LINES_BY_FILE} - /logs/packets-${TIMESTAMP}-
